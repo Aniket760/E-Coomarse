8fdbcf71ea5c2be7f2dba8aa92d3040fea2f42b3
Aniket Gupta <aniketgupta@Anikets-MacBook-Air.local>
2026-02-23 22:09:07 +0530

    Set STATIC_ROOT for Render collectstatic

--- ecommarse_site/settings.py
+++ ecommarse_site/settings.py
@@ -68,6 +68,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "storefront" / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
